60c1faefb7a79f154be9668057c9dcdccff130a5
Pavel <venom200889@gmail.com>
2019-12-08 16:23:52 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,24 @@
+'use strict';м  //стандарт ES6
+
+let money = prompt("Ваш бюджет за месяц?", " ");
+let time = prompt("Введите дату в формате YYYY-MM-DD", "YYYY-MM-DD");
+let obj = {};
+console.log(money);
+console.log(time);
+
+
+ let appData = {
+   let budjet =  money  ,
+   time: ,
+   expenses : {
+   “ответ на первый вопрос” : “ответ на второй вопрос”
+      }
+    ,
+   optionalExpenses : {},
+   income : [],
+   savings : false  
+}; 
+
+
+/*alert(1)
+[0,3].push()*/
